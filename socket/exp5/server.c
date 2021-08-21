0e401b3475e5acb952e0a245aba3b3b99188b404
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2021-08-21 22:04:26 +0530

    ✨ exp:5 daytime TCP server

---
+++ socket/exp5/server.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<time.h>
+#include<string.h>
+int main()
+{
+	time_t t;   
+  time(&t);
+	char serv_mess[100];
+	strcpy(serv_mess,ctime(&t));
+  // printf("\nThis program has been writeen at (date and time): %s", serv_mess);
+	int server_socket;
+	server_socket = socket(AF_INET,SOCK_STREAM,0);
+
+//DEFINE THE SERVER ADDRESS
+  struct sockaddr_in server_address;
+  server_address.sin_family=AF_INET;
+  server_address.sin_port = htons(2011);
+  server_address.sin_addr.s_addr=INADDR_ANY;
+  
+//Bind the socket to our specified port and IP address
+
+  bind(server_socket,(struct sockaddr*) &server_address,sizeof(server_address));
+  listen(server_socket,5);
+  int client_socket;
+  client_socket = accept(server_socket,NULL,NULL);
+
+  //Send the message
+  send(client_socket,serv_mess,sizeof(serv_mess),0);
+  return 0;
+}
