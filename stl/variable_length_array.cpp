0b68fed7dd26fec86018fc1a2be7e54ae23e4648
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2021-12-24 13:59:18 +0530

    run code

---
+++ stl/variable_length_array.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int n, q;
+    vector<int> v{10, 20, 30};
+    cout << v.size();
+    return 0;
+}
